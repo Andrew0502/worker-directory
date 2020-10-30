58072117ee434f6aea3462fe139dbc3fbcaa354a
Andrew0502 <agcmarcus@gmail.com>
2020-10-30 17:39:34 -0400

    Added utils folder and API.jsx in that folder to make an api call.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SearchResultContainer from "./components/SearchResultContainer";
+// import ResultList from "./components/ResultList";
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <header
+        style={{
+          backgroundColor: "rgb(0,24,65)",
+          height: 130,
+          borderBottom: "5px solid red",
+        }}
+        className="header"
+      >
+        <h1
+          style={{
+            color: "white",
+            textAlign: "center",
+            fontFamily: "Reem Kufi",
+          }}
         >
-          Learn React
-        </a>
+          Employee Directory
+        </h1>
+        <p
+          style={{
+            color: "white",
+            textAlign: "center",
+            fontFamily: "Reem Kufi",
+          }}
+        >
+          Click on carrots to filter by heading or use the search box to narrow
+          your results.
+        </p>
       </header>
-    </div>
+      {/* <div style={{ margin: 20, display: "flex", justifyContent: "center" }}>
+        <input
+          style={{
+            height: 45,
+            width: 200,
+            borderRadius: 5,
+            borderColor: "#E8E7EB",
+          }}
+          type="search"
+          id="search"
+          placeholder="Search"
+          fontFamily="Reem Kufi"
+          onChange={props.handleInputChange}
+          value={props.value}
+        ></input>
+      </div> */}
+      <div>
+                 <SearchResultContainer />
+            </div>
+    </>
   );
 }
 
